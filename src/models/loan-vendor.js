d33e3cf83bf6650033aa4154a0d14c8441596543
Siddharth <siddharthsingh2014@gmail.com>
2024-05-11 23:25:48 +0530

    FEAT: Added loanVendor Schema

---
+++ src/models/loan-vendor.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const loanVendorSchema = new mongoose.Schema(
+    {
+        client_id: {
+            type: mongoose.Schema.ObjectId,
+            required: [true, "Please enter your client id"]
+        },
+        name: {
+            type: String,
+        },
+        logo: {
+            type: String,
+        },
+        approval_rate: {
+            type: String,
+            enum: ["Bad", "Medium", "Excellent"]
+        },
+        max_loan_amount: {
+            type: Number,
+        },
+        min_rate: {
+          type: Number,
+        },
+        tenure_in_months: {
+          type: Number,
+        },
+        processing_fee: {
+          type: Number,
+        },
+        redirect_url: {
+          type: String,
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const LoanVendor = mongoose.models.loanVendor || mongoose.model("loanVendor", loanVendorSchema);
