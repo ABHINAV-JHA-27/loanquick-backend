51cb55ba178311706de1becee2b1e9b1a2a79746
Siddharth <siddharthsingh2014@gmail.com>
2024-05-11 21:13:11 +0530

    FIX: Changed URL from .com to .in

--- src/utils/zoho-endpoints.js
+++ src/utils/zoho-endpoints.js
@@ -4,6 +4,7 @@ export const endpoints = {
     getTokens: "https://accounts.zoho.in/oauth/v2/token",
     getAccessToken: "https://accounts.zoho.in/oauth/v2/token",
     revokeRefreshToken: "https://accounts.zoho.in/oauth/v2/token/revoke",
-    getExpenses: "https://www.zohoapis.com/books/v3/expenses",
-    getInvoices: "https://www.zohoapis.com/books/v3/invoices",
+    getExpenses: "https://www.zohoapis.in/books/v3/expenses",
+    getInvoices: "https://www.zohoapis.in/books/v3/invoices",
+    getBills: "https://www.zohoapis.in/books/v3/bills"
 };
