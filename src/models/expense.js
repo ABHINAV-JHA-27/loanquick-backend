8fb727d50770e9aa2a7a0aab2d1a2ee17d6c61f5
Siddharth <siddharthsingh2014@gmail.com>
2024-05-11 23:44:46 +0530

    FEAT: Added Expense Schema

---
+++ src/models/expense.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const Schema = new mongoose.Schema(
+    {
+        client_id: {
+            type: mongoose.Schema.ObjectId,
+            required: [true, "Please enter your client id"]
+        },
+        vendor_id: {
+            type: String,
+        },
+        vendor_name: {
+            type: String,
+        },
+        total: {
+            type: Number,
+        },
+        date: {
+            type: String,
+        },
+        expense_type: {
+            type: String,
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const Expense = mongoose.models.expense || mongoose.model("expense", expenseSchema);
